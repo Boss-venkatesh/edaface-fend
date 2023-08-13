9f7bdd2ee48f12553791ad1d7accea4c7fb2a402
venkat <88572917+Boss-venkatesh@users.noreply.github.com>
2023-08-13 09:43:48 +0530

    Update Recently.jsx

--- edaface/src/components/Recently.jsx
+++ edaface/src/components/Recently.jsx
@@ -23,7 +23,7 @@ function Recently() {
         <div className="row mt-4">
         <div className="col-lg-3 col-md-6 col-12">
              <div class="card card-recently mt-2" >
-               <img src={message} class="img-fluid" alt="..." className='recently-card-banner' />
+               <img src={bannergirl} class="img-fluid" alt="..." className='recently-card-banner' />
               <div class="card-body">
                 <h6 class="card-title"><span className='card-recently-head'>Women NFT #325</span>
                 <span className='card-star-icons'>
@@ -57,42 +57,7 @@ function Recently() {
 
             <div className="col-lg-3 col-md-6 col-12">
              <div class="card card-recently mt-2" >
-               <img src={message} class="img-fluid" alt="..." className='recently-card-banner' />
-              <div class="card-body">
-                <h6 class="card-title"><span className='card-recently-head'>Women NFT #325</span>
-                <span className='card-star-icons'>
-                  <FaStar style={{color:'#FDC206'}}/>
-                  <FaStar style={{color:'#FDC206'}}/>
-                  <FaStar style={{color:'#FDC206'}}/>
-                  <FaStar/>
-                  <FaStar/>
-                  </span></h6><br />
-                <p class="card-text"><span className='highrecently'>Hight Bid</span> <span className='amtrecently'>15,000 BUSD</span></p>
-              </div>
-              
-              <div className="row">
-                <div className="col-lg-1 col-md-1 col-1">
-                  <img src={live}  className="rounded-circle recently-round-img1 mx-2" style={{width:'25px'}} alt="" />
-                </div>
-                <div className="col-lg-1 col-md-1 col-1">
-                  <img src={live}  className="rounded-circle recently-round-img2 " style={{width:'25px'}} alt="" />
-                </div>
-                <div className="col-lg-9 col-md-9 col-9">
-                    <div className="recently-body">
-                      <h6>@ robert03</h6>
-                      <p><span>by lorem ipsum</span> 
-                      <span className='recently-body-icons'><FaHeart/><i className='mx-1'>246</i></span></p>
-                    </div>
-                </div>
-              
-               </div>
-             </div>
-            </div>
-
-
-            <div className="col-lg-3 col-md-6 col-12">
-             <div class="card card-recently mt-2" >
-               <img src={message} class="img-fluid" alt="..." className='recently-card-banner' />
+               <img src={bannergirl} class="img-fluid" alt="..." className='recently-card-banner' />
               <div class="card-body">
                 <h6 class="card-title"><span className='card-recently-head'>Women NFT #325</span>
                 <span className='card-star-icons'>
@@ -127,7 +92,42 @@ function Recently() {
 
             <div className="col-lg-3 col-md-6 col-12">
              <div class="card card-recently mt-2" >
-               <img src={message} class="img-fluid" alt="..." className='recently-card-banner' />
+               <img src={bannergirl} class="img-fluid" alt="..." className='recently-card-banner' />
+              <div class="card-body">
+                <h6 class="card-title"><span className='card-recently-head'>Women NFT #325</span>
+                <span className='card-star-icons'>
+                  <FaStar style={{color:'#FDC206'}}/>
+                  <FaStar style={{color:'#FDC206'}}/>
+                  <FaStar style={{color:'#FDC206'}}/>
+                  <FaStar/>
+                  <FaStar/>
+                  </span></h6><br />
+                <p class="card-text"><span className='highrecently'>Hight Bid</span> <span className='amtrecently'>15,000 BUSD</span></p>
+              </div>
+              
+              <div className="row">
+                <div className="col-lg-1 col-md-1 col-1">
+                  <img src={live}  className="rounded-circle recently-round-img1 mx-2" style={{width:'25px'}} alt="" />
+                </div>
+                <div className="col-lg-1 col-md-1 col-1">
+                  <img src={live}  className="rounded-circle recently-round-img2 " style={{width:'25px'}} alt="" />
+                </div>
+                <div className="col-lg-9 col-md-9 col-9">
+                    <div className="recently-body">
+                      <h6>@ robert03</h6>
+                      <p><span>by lorem ipsum</span> 
+                      <span className='recently-body-icons'><FaHeart/><i className='mx-1'>246</i></span></p>
+                    </div>
+                </div>
+              
+               </div>
+             </div>
+            </div>
+
+
+            <div className="col-lg-3 col-md-6 col-12">
+             <div class="card card-recently mt-2" >
+               <img src={bannergirl} class="img-fluid" alt="..." className='recently-card-banner' />
               <div class="card-body">
                 <h6 class="card-title"><span className='card-recently-head'>Women NFT #325</span>
                 <span className='card-star-icons'>
